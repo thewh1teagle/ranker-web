0cfdb51d7ad0c5aac06eca8022f8bed917d23b6b
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-01-29 00:10:18 +0200

    add icons

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         <Meta />
         <Links />
       </head>
